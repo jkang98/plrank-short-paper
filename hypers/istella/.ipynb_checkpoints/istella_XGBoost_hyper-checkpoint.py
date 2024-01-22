a2cae5bf6da8dfb7ee2d0a9250dd0ab42882ea1c
jkang98 <jingweids@outlook.com>
2024-01-22 09:58:43 +0100

    Update

--- hypers/istella/.ipynb_checkpoints/istella_XGBoost_hyper-checkpoint.py
+++ hypers/istella/.ipynb_checkpoints/istella_XGBoost_hyper-checkpoint.py
@@ -397,166 +397,181 @@ def gumbel_sample_rankings(
     return rankings
 
 
+def log_safe_zeros(values):
+    result = np.full(values.shape, np.NINF)
+    np.log(values, out=result, where=np.not_equal(values, 0))
+    return result
+
 def PL_rank_3_grad(rank_weights, labels, scores, n_samples):
     n_docs = labels.shape[0]
     cutoff = min(rank_weights.shape[0], n_docs)
 
+
     sampled_rankings = gumbel_sample_rankings(
-        scores, n_samples, cutoff=cutoff, return_full_rankings=True
-    )
-
+            scores, n_samples, cutoff=cutoff, return_full_rankings=True
+        )
+    
     cutoff_sampled_rankings = sampled_rankings[:, :cutoff]
-
-    scores = scores - np.max(scores) + 10.0
+    
+    scores = scores.copy() - np.amax(scores) + 10.0
 
     srange = np.arange(n_samples)
-    crange = np.arange(cutoff)
 
     weighted_labels = labels[cutoff_sampled_rankings] * rank_weights[None, :cutoff]
     cumsum_labels = np.cumsum(weighted_labels[:, ::-1], axis=1)[:, ::-1]
 
-    # first order
-    result1 = np.zeros(n_docs, dtype=np.float64)
-    np.add.at(result1, cutoff_sampled_rankings[:, :-1], cumsum_labels[:, 1:])
-    result1 /= n_samples
+    result = np.zeros(n_docs, dtype=np.float64)
+    np.add.at(result, cutoff_sampled_rankings[:, :-1], cumsum_labels[:, 1:])
+    result /= n_samples
+    
+    in_top_k = np.zeros((n_samples, n_docs), dtype=bool)
+    in_top_k[srange[:, None], cutoff_sampled_rankings] = True
 
-    exp_scores = np.exp(scores).astype(np.float64)
-    denom_per_rank = np.cumsum(exp_scores[sampled_rankings[:, ::-1]], axis=1)[
-        :, : -cutoff - 1 : -1
-    ]
+    log_denom_per_rank = np.logaddexp.accumulate(
+        scores[sampled_rankings[:, ::-1]], axis=1
+    )[:, : -cutoff - 1 : -1]
 
-    # DR
-    cumsum_weight_denom = np.cumsum(rank_weights[:cutoff] / denom_per_rank, axis=1)
-    # RI
-    cumsum_reward_denom = np.cumsum(cumsum_labels / denom_per_rank, axis=1)
+    log_rank_weights = log_safe_zeros(rank_weights[:cutoff])
+    log_cumsum_labels = log_safe_zeros(cumsum_labels)
+    log_cumsum_weight_denom = np.logaddexp.accumulate(
+        log_rank_weights - log_denom_per_rank, axis=1
+    )
+    log_cumsum_reward_denom = np.logaddexp.accumulate(
+        log_cumsum_labels - log_denom_per_rank, axis=1
+    )
 
     relevant_docs = np.where(np.not_equal(labels, 0))[0]
     if cutoff < n_docs:
-        second_part = -exp_scores[None, :] * cumsum_reward_denom[:, -1, None]
-        second_part[:, relevant_docs] += (
-            labels[relevant_docs][None, :]
-            * exp_scores[None, relevant_docs]
-            * cumsum_weight_denom[:, -1, None]
+        safe_scores = np.repeat(scores[None, :], n_samples, axis=0)
+        safe_scores[in_top_k] = np.min(scores)
+        second_part = -np.exp(safe_scores + log_cumsum_reward_denom[:, -1, None])
+        second_part[:, relevant_docs] += labels[relevant_docs][None, :] * np.exp(
+            safe_scores[:, relevant_docs] + log_cumsum_weight_denom[:, -1, None]
         )
     else:
         second_part = np.empty((n_samples, n_docs), dtype=np.float64)
 
-    sampled_direct_reward = (
-        labels[cutoff_sampled_rankings]
-        * exp_scores[cutoff_sampled_rankings]
-        * cumsum_weight_denom
+    sampled_direct_reward = labels[cutoff_sampled_rankings] * np.exp(
+        scores[cutoff_sampled_rankings] + log_cumsum_weight_denom
     )
-    sampled_following_reward = exp_scores[cutoff_sampled_rankings] * cumsum_reward_denom
+    sampled_following_reward = np.exp(
+        scores[cutoff_sampled_rankings] + log_cumsum_reward_denom
+    )
     second_part[srange[:, None], cutoff_sampled_rankings] = (
         sampled_direct_reward - sampled_following_reward
     )
 
-    return -(result1 + np.mean(second_part, axis=0))
+    return -(result + np.mean(second_part, axis=0))
+
 
 def PL_rank_3_hess(rank_weights, labels, scores, n_samples):
     n_docs = labels.shape[0]
     cutoff = min(rank_weights.shape[0], n_docs)
-    relevant_docs = np.where(np.not_equal(labels, 0))[0]
 
     sampled_rankings = gumbel_sample_rankings(
-        scores, n_samples, cutoff=cutoff, return_full_rankings=True
-    )
-
+            scores, n_samples, cutoff=cutoff, return_full_rankings=True
+        )
     cutoff_sampled_rankings = sampled_rankings[:, :cutoff]
 
-    scores = scores - np.max(scores) + 10.0
+    scores = scores.copy() - np.amax(scores) + 10.0
 
     srange = np.arange(n_samples)
-    crange = np.arange(cutoff)
 
     weighted_labels = labels[cutoff_sampled_rankings] * rank_weights[None, :cutoff]
     cumsum_labels = np.cumsum(weighted_labels[:, ::-1], axis=1)[:, ::-1]
 
-    exp_scores = np.exp(scores).astype(np.float64)
-    denom_per_rank = np.cumsum(exp_scores[sampled_rankings[:, ::-1]], axis=1)[
-        :, : -cutoff - 1 : -1
-    ]
+    in_top_k = np.zeros((n_samples, n_docs), dtype=bool)
+    in_top_k[srange[:, None], cutoff_sampled_rankings] = True
+    
+    safe_scores = np.repeat(scores[None, :], n_samples, axis=0)
+    safe_scores[in_top_k] = np.min(scores)
 
-    # second order
-    result2 = np.zeros(n_docs, dtype=np.float64)
+    log_denom_per_rank = np.logaddexp.accumulate(
+        scores[sampled_rankings[:, ::-1]], axis=1
+    )[:, : -cutoff - 1 : -1]
+    log_cumsum_denom = np.logaddexp.accumulate(0 - log_denom_per_rank, axis=1)
+
+    result = np.zeros(n_docs, dtype=np.float64)
     np.add.at(
-        result2,
+        result,
         cutoff_sampled_rankings[:, :-1],
         cumsum_labels[:, 1:]
         * (
             1
-            - exp_scores[cutoff_sampled_rankings[:, :-1]]
-            * np.cumsum(1 / denom_per_rank[:, :-1], axis=1)
+            - np.exp(scores[cutoff_sampled_rankings[:, :-1]] + log_cumsum_denom[:, :-1])
         ),
     )
-    result2 /= n_samples
+    result /= n_samples
 
-    cumsum_denom = np.cumsum(1 / denom_per_rank, axis=1)
-    sum_prob_per_doc = exp_scores * cumsum_denom[:, -1, None]
-    sum_prob_per_doc[srange[:, None], cutoff_sampled_rankings] = (
-        exp_scores[cutoff_sampled_rankings] * cumsum_denom
+    sum_prob_per_doc = np.exp(safe_scores + log_cumsum_denom[:, -1, None])
+    sum_prob_per_doc[srange[:, None], cutoff_sampled_rankings] = np.exp(
+        scores[cutoff_sampled_rankings] + log_cumsum_denom
     )
 
     in_or_not = np.zeros((n_samples, n_docs), dtype=np.float64)
     in_or_not[srange[:, None], cutoff_sampled_rankings] = 1
     long_item = in_or_not + 1 - sum_prob_per_doc
 
-    # DR
-    cumsum_weight_denom = np.cumsum(rank_weights[:cutoff] / denom_per_rank, axis=1)
-    # RI
-    cumsum_reward_denom = np.cumsum(cumsum_labels / denom_per_rank, axis=1)
-
+    # second part
+    log_rank_weights = log_safe_zeros(rank_weights[:cutoff])
+    log_cumsum_labels = log_safe_zeros(cumsum_labels)
+    log_cumsum_weight_denom = np.logaddexp.accumulate(
+        log_rank_weights - log_denom_per_rank, axis=1
+    )
+    log_cumsum_reward_denom = np.logaddexp.accumulate(
+        log_cumsum_labels - log_denom_per_rank, axis=1
+    )
+    
+    
+    relevant_docs = np.where(np.not_equal(labels, 0))[0]
     if cutoff < n_docs:
-        second_part = -exp_scores[None, :] * cumsum_reward_denom[:, -1, None]
-        second_part[:, relevant_docs] += (
-            labels[relevant_docs][None, :]
-            * exp_scores[None, relevant_docs]
-            * cumsum_weight_denom[:, -1, None]
+        second_part = -np.exp(safe_scores + log_cumsum_reward_denom[:, -1, None])
+        second_part[:, relevant_docs] += labels[relevant_docs][None, :] * np.exp(
+            safe_scores[:, relevant_docs] + log_cumsum_weight_denom[:, -1, None]
         )
     else:
         second_part = np.empty((n_samples, n_docs), dtype=np.float64)
 
-    sampled_direct_reward = (
-        labels[cutoff_sampled_rankings]
-        * exp_scores[cutoff_sampled_rankings]
-        * cumsum_weight_denom
+    sampled_direct_reward = labels[cutoff_sampled_rankings] * np.exp(
+        scores[cutoff_sampled_rankings] + log_cumsum_weight_denom
     )
-    sampled_following_reward = exp_scores[cutoff_sampled_rankings] * cumsum_reward_denom
+    sampled_following_reward = np.exp(
+        scores[cutoff_sampled_rankings] + log_cumsum_reward_denom
+    )
     second_part[srange[:, None], cutoff_sampled_rankings] = (
         sampled_direct_reward - sampled_following_reward
     )
-
-    # DR
-    cumsum_weight_denom_square = np.cumsum(
-        rank_weights[:cutoff] / denom_per_rank ** 2, axis=1
+    
+    
+    # third part
+    log_cumsum_weight_denom_square = np.logaddexp.accumulate(
+        log_rank_weights - 2*log_denom_per_rank, axis=1
     )
-    # RI
-    cumsum_reward_denom_square = np.cumsum(cumsum_labels / denom_per_rank ** 2, axis=1)
+    log_cumsum_reward_denom_square = np.logaddexp.accumulate(
+        log_cumsum_labels - 2*log_denom_per_rank, axis=1
+    )
 
     if cutoff < n_docs:
-        third_part = -exp_scores[None, :] ** 2 * cumsum_reward_denom_square[:, -1, None]
-        third_part[:, relevant_docs] += (
-            labels[relevant_docs][None, :]
-            * exp_scores[None, relevant_docs] ** 2
-            * cumsum_weight_denom_square[:, -1, None]
+        third_part = -np.exp(2*safe_scores + log_cumsum_reward_denom_square[:, -1, None])
+        third_part[:, relevant_docs] += labels[relevant_docs][None, :] * np.exp(
+            2 * safe_scores[:, relevant_docs] + log_cumsum_weight_denom_square[:, -1, None]
         )
     else:
         third_part = np.empty((n_samples, n_docs), dtype=np.float64)
 
-    sampled_direct_reward_square = (
-        labels[cutoff_sampled_rankings]
-        * exp_scores[cutoff_sampled_rankings] ** 2
-        * cumsum_weight_denom_square
+
+    sampled_direct_reward_square = labels[cutoff_sampled_rankings] * np.exp(
+        2*scores[cutoff_sampled_rankings] + log_cumsum_weight_denom_square
     )
-    sampled_following_reward_square = (
-        exp_scores[cutoff_sampled_rankings] ** 2 * cumsum_reward_denom_square
+    sampled_following_reward_square = np.exp(
+        2 * scores[cutoff_sampled_rankings] + log_cumsum_reward_denom_square
     )
     third_part[srange[:, None], cutoff_sampled_rankings] = (
         sampled_direct_reward_square - sampled_following_reward_square
     )
 
     return -(
-        result2 + np.mean(second_part * long_item, axis=0) - np.mean(third_part, axis=0)
+        result + np.mean(second_part * long_item, axis=0) - np.mean(third_part, axis=0)
     )
 
 def plrank3obj(preds, dtrain):
@@ -672,7 +687,7 @@ new_valid.set_group([data.validation.query_feat(i).shape[0] for i in range(valid
 def objective(trial):
     params = {
         "verbosity": 0,
-        "learning_rate": trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True),
+        "learning_rate": trial.suggest_float("learning_rate", 1e-4, 1, log=True),
         "max_depth": trial.suggest_int("max_depth", 1, 8),
         "min_child_weight": trial.suggest_int("min_child_weight", 2, 8),
         "gamma": trial.suggest_float("gamma", 1e-8, 1.0, log=True),
@@ -682,11 +697,11 @@ def objective(trial):
     bst = xgb.train(
         params,
         new_train,
-        num_boost_round=30,
+        num_boost_round=200,
         evals=[(new_train, "train"), (new_valid, "vali")],
         obj=plrank3obj,
         custom_metric=ndcg_dataset,
-        #early_stopping_rounds=2,
+        early_stopping_rounds=20,
         verbose_eval=False,
     )
 
@@ -695,7 +710,7 @@ def objective(trial):
 
 # Run hyperparameter search
 study = optuna.create_study(direction="minimize")
-study.optimize(objective, n_trials=100, timeout=2*60*60)
+study.optimize(objective, n_trials=100, timeout=12*60*60)
 
 print("Number of finished trials: ", len(study.trials))
 print("Best trial:")
